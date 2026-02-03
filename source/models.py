5f922cf6cde870b29d286180d0420a0cf5c82c63
Dezsoe Babai <de.babai2000@gmail.com>
2026-02-03 15:51:53 +0100

    Add notebook for flow matching on a torus
    Signed-off-by: Dezsoe Babai <de.babai2000@gmail.com>

--- source/models.py
+++ source/models.py
@@ -9,3 +9,17 @@ class SimpleFlow(nn.Module):
 
     def forward(self, x_t, t):
         return self.model(torch.cat([x_t, t], dim=1))
+
+from e3nn.o3 import FullyConnectedTensorProduct
+from e3nn.nn import FullyConnectedNet, Gate
+
+class EquivariantFlow(nn.Module):
+    def __init__(self, dim=2):
+        super().__init__()
+        self.model = nn.Sequential(
+            FullyConnectedNet([dim + 1, 64, 64], torch.relu),
+            nn.Linear(64, dim)  # output tangent vector
+        )
+
+    def forward(self, x_t, t):
+        return self.model(torch.cat([x_t, t], dim=1))
